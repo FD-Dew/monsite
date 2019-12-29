d63d6206c179aa9f6e8ba8bc9bd54a6657613de3
FD-Dew <58472819+FD-Dew@users.noreply.github.com>
2019-12-29 21:03:02 +0100

    Add files via upload

---
+++ includes/menu.inc.php
@@ -0,0 +1,39 @@
+
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">Start Bootstrap</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php">Home <!-- Redirige vers le fichier la page d'accueil -->
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="article.php">Article</a> <!-- Redirige vers la page article -->
+          </li>
+
+
+          <li class="nav-item">
+            <a class="nav-link" href="adduser.php">Utilisateur</a> <!-- Redirige vers la page de création d'utilisateur -->
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="login.php">Connexion</a> <!-- Redirige vers la page de connexion -->
+          </li>
+
+		   <li class="nav-item">
+            <a class="nav-link" href="deconnexion.php">Deconnexion</a><!-- Redirige vers la page de deconnexion -->
+          </li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
---
+++ templates_c/efd9381d16e3df2f3f9945c0f75b42a48ea9b7a8_0.file.index.tpl.php
@@ -0,0 +1,26 @@
+<?php
+/* Smarty version 3.1.33, created on 2019-11-12 15:51:00
+  from 'C:\xampp\htdocs\monsite\index.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5dcac6d48590e5_14949389',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'efd9381d16e3df2f3f9945c0f75b42a48ea9b7a8' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\monsite\\index.tpl',
+      1 => 1573570215,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5dcac6d48590e5_14949389 (Smarty_Internal_Template $_smarty_tpl) {
+?><p>Bonjour <?php echo $_smarty_tpl->tpl_vars['name']->value;?>
+</p><?php }
+}
